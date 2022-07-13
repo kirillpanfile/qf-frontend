24b0dc32fa59100a53e048ba87701d359c319fc9
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-07-13 20:24:21 +0300

    project init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+//imports
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//styles
+import "./scss/style.scss";
+
+//create app
 createApp(App).use(store).use(router).mount("#app");
