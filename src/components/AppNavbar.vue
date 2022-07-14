32d41039fdbbf5def693807dbc7f50a6f0629494
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-07-14 21:08:57 +0300

    Created nav-bar

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="navbar">
+        <div class="navbar__wrapper">
+            <div class="navbar__logo">
+                <img src="@/assets/Header.svg" />
+            </div>
+            <div class="navbar__content">
+                <h1>Categories</h1>
+                <h1>Home</h1>
+                <h1>Search</h1>
+                <h1>Explore</h1>
+                <div class="navbar__user">
+                    ? <!--! user's image -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
