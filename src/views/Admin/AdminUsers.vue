cc700eaf1888c5a722441be99f7bd693b78afb9b
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-07-16 14:55:47 +0300

    created card

--- src/views/Admin/AdminUsers.vue
+++ src/views/Admin/AdminUsers.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>Admin Users</div>
+    <div class="admin-user__wrapper">
+        <div class="admin-user__card">
+            <input type="checkbox">
+            <div class="admin-user__card-content">
+                <div class="admin-user__info">   
+                    <h1>Username</h1>
+                    <div class="admin-user__line"></div>
+                    <h1>Email</h1>
+                    <div class="admin-user__line"></div>
+                    <h1>Roles: ''</h1>
+                </div>
+                <div class="admin-user__buttons">
+                    <button class="admin-user__button btn-primary">
+                        Edit
+                    </button>
+                    <button class="admin-user__button btn-danger">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
