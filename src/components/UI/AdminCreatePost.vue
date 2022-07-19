85d04faffc266be0d705942eba5b141384a4ee93
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-07-19 16:55:44 +0300

    createpost fix

--- src/components/UI/AdminCreatePost.vue
+++ src/components/UI/AdminCreatePost.vue
@@ -1,61 +1,130 @@
 <template>
-  <div class="admin-create-post">
-    <div class="admin-create-post__wrapper">
-      <div class="admin-create-post__left">
-        <input type="text" placeholder="Name..." />
-        <textarea cols="30" rows="10" placeholder="Description..."></textarea>
-        <div class="admin-create-post__preparation">
-          <label for="step">Preparation Steps:</label>
-          <div>
-            <textarea
-              v-model="step"
-              id="step"
-              type="text"
-              placeholder="Step..."
-            />
-            <button class="admin-create-post__button" @click="inputStep">
-              Add Step
-            </button>
-          </div>
-          <div class="admin-create-post__empty" v-if="steps.length == 0">
-            There are no steps <br />
-            Add some
-          </div>
-          <ul v-else>
-            <li v-for="(item, index) in steps" :key="index">
-              <strong>{{ index + 1 }}.</strong> {{ item }}
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="admin-create-post__right">
-        <input class="input-image" type="file" />
-        <div class="admin-create-post__ingredients">
-          <input type="text" placeholder="Enter a ingredient..." />
-        </div>
-      </div>
+    <div class="admin-post">
+        <h1 class="admin-post__title">Create a new post</h1>
+        <form class="admin-post__form" @submit.prevent="createPost">
+            <div class="admin-post__form-group">
+                <label for="title">Title</label>
+                <input placeholder="title" type="text" id="title" v-model="title" required />
+            </div>
+            <div class="admin-post__form-group">
+                <div class="">Description</div>
+                <textarea placeholder="description" id="content" v-model="content" required></textarea>
+            </div>
+            <div class="admin-post__form-group">
+                <label for="category">Category</label>
+                <select id="category" v-model="category" required>
+                    <option value="">Select a category</option>
+                    <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
+                </select>
+            </div>
+            <div class="admin-post__form-group">
+                <label for="ingredients">Ingredients</label>
+                <input
+                    placeholder="Ingredients"
+                    type="text"
+                    @keypress="searchValue"
+                    id="ingredients"
+                    v-model="searchInput"
+                    required
+                />
+                <ul class="admin-search" v-if="search">
+                    <li v-for="tag in ingredients" class="admin-search__item">{{ tag }}</li>
+                </ul>
+            </div>
+            <div class="admin-post__form-group">
+                <label for="image">Image</label>
+                <input placeholder="Select Image" type="file" id="image" required />
+            </div>
+            <div class="admin-post__form-group">
+                <button type="submit">Create</button>
+            </div>
+        </form>
     </div>
-    <button class="admin-create-post__submit">Submit</button>
-  </div>
 </template>
 
 <script>
-export default {
-  name: "AdminCreatePost",
-  data() {
-    return {
-      steps: [],
-      step: "",
-    };
-  },
-  methods: {
-    inputStep() {
-      if (this.step.length > 0) {
-        this.steps.push(this.step);
-        this.step = "";
-      }
-    },
-  },
-};
+const name = 'AdminCreatePost'
+export default { name }
 </script>
+
+<script setup>
+import { ref, watch } from 'vue'
+const title = ref(''),
+    content = ref(''),
+    category = ref(''),
+    ingredients = ref([
+        {
+            id: 1,
+            name: 'Tomato',
+        },
+        {
+            id: 2,
+            name: 'Cheese',
+        },
+        {
+            id: 3,
+            name: 'Onion',
+        },
+        {
+            id: 4,
+            name: 'Garlic',
+        },
+        {
+            id: 5,
+            name: 'Parsley',
+        },
+        {
+            id: 6,
+            name: 'Salt',
+        },
+        {
+            id: 7,
+            name: 'Pepper',
+        },
+        {
+            id: 8,
+            name: 'celery',
+        },
+        {
+            id: 9,
+            name: 'carrot',
+        },
+        {
+            id: 10,
+            name: 'cucumber',
+        },
+        {
+            id: 11,
+            name: 'mushroom',
+        },
+        {
+            id: 12,
+            name: 'olive',
+        },
+    ]),
+    categories = ref([
+        { id: 1, name: 'News' },
+        { id: 2, name: 'Events' },
+        { id: 3, name: 'About' },
+        { id: 4, name: 'Contact' },
+        { id: 5, name: 'Other' },
+    ])
+
+const search = ref(false)
+const searchInput = ref('')
+
+const timer = ref(null)
+
+const searchValue = (e) => {
+    clearTimeout(timer.value)
+    timer.value = setTimeout(() => {
+        search.value = true
+    }, 500)
+}
+
+watch(searchInput, (value) => {
+    if (value.length < 1) search.value = false
+})
+</script>
+
+<style></style>
