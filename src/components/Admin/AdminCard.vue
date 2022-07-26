20b356a714d637c2f7dc5ecc1fb73f21c83d6655
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-07-26 13:49:03 +0300

    finished card component

--- src/components/Admin/AdminCard.vue
+++ src/components/Admin/AdminCard.vue
@@ -1,26 +1,41 @@
 <template>
-    <div class="card__container">
-        <div class="card__header">
-            <div class="card__hero">
-                <div class="card__image"> <!--TODO imaginea e statica, de facut reactive-->
-                    <img src="@/assets/cookies.jpg" alt="recipe-image" /> <!--? Image--> 
-                </div>
-                <div class="card__title">
-                    <h1>{{ title }}</h1> <!--? Title--> 
-                    <div class="card__temperature" v-if="temperature"> <!--? Temperature--> 
-                        <h2>HOT</h2> 
-                        <img src="@/assets/fire.svg" alt="recipe-temp" />
-                    </div>
-                </div>
-                <div class="card__description">
-                    {{ description }}
+    <div class="card__container flex-g20">
+        <div class="card__content"> <!--? Left side of card -->
+            <div class="card__image"> <!--? Card image -->
+                <img src="@/assets/cookies.jpg" alt="card-image" />
+            </div>
+            <div class="card__title">
+                <h1>{{ title }}</h1>
+                <div class="card__temperature" v-if="temperature"><!--? Temperature-->
+                    <h2>HOT</h2>
+                    <img src="@/assets/fire.svg" alt="recipe-temp" />
                 </div>
             </div>
+            <div class="card__description">
+                {{ description }}
+            </div>
+            <div class="card__steps steps">
+                <div class="steps__title">
+                    <h1>Preparation</h1>
+                    <div class="steps__line"></div>
+                    <div class="steps__time">
+                        <img src="@/assets/timeOff.svg" alt="steps-time">
+                        <h2>{{ time }} mins</h2> <!--? Time-->
+                    </div>
+                </div>
+                <div class="steps__step" v-for="(step,index) in steps" :key="index">
+                    <h3>{{index + 1}}</h3>
+                    <h4>{{step}}</h4> <!--? Steps-->
+                </div>
+            </div>
+        </div>
+
+        <div class="card__right">
             <div class="card__ingredients">
                 <div class="card__cells"> <!--? small Cells--> 
                     <div class="cell" v-for="item in cells">{{ item }}</div>
                 </div>
-                <div class="flex-5">
+                <div class="flex-g5">
                     <h1>Ingredients</h1>
                     <img src="@/assets/chef.svg" alt="recipe-ingredients" />
                 </div>
@@ -35,25 +50,18 @@
                     <h1>Add to favourites</h1> 
                 </button>
             </div>
-        </div>
-        <div class="card__content">
-            <div class="card__steps steps">
-                <div class="steps__title">
-                    <h1>Preparation</h1>
-                    <div class="steps__line"></div>
-                    <div class="steps__time">
-                        <img src="@/assets/timeOff.svg" alt="steps-time">
-                        <h2>{{ time }} mins</h2> <!--? Time-->
-                    </div>
+            <div class="card__featured featured">
+                <div class="featured__title">
+                    <h1>Featured</h1>
+                    <img src="@/assets/featured.svg" alt="featured-icon">
                 </div>
-                <div class="steps__step" v-for="(step,index) in steps" :key="index">
-                    <h3>{{index + 1}}</h3>
-                    <h4>{{step}}</h4>
-                </div>
+                <div class="featured__content">
+                    <div class="featured__card">Content</div>
+                    <div class="featured__card">Content</div>
 
+                </div>
             </div>
-            <div class="card__featured"></div>
-        </div>
+        </div> <!--? Right side of card-->
     </div>
 </template>
 
@@ -68,11 +76,11 @@ defineProps({
         required: true,
     },
     description: {
-        type: String
+        type: String,
     },
     temperature: {
         type: Boolean,
-        default: false
+        default: false,
     },
     cells: {
         type: Array,
@@ -82,11 +90,11 @@ defineProps({
     },
     time: {
         type: Number,
-        required: true
+        required: true,
     },
     steps: {
         type: Array,
-        required: true
-    }
+        required: true,
+    },
 })
 </script>
