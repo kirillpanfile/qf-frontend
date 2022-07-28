2bad2f63f773b1ae6dc4b910fd97538cf9c1ee53
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-07-28 22:14:55 +0300

    Update config.js

--- src/store/config.js
+++ src/store/config.js
@@ -1,5 +1,5 @@
-// const URL = 'https://99gapsspx1.execute-api.eu-central-1.amazonaws.com'
-const URL = 'http://localhost:3000'
+const URL = 'https://99gapsspx1.execute-api.eu-central-1.amazonaws.com'
+// const URL = 'http://localhost:3000'
 const jwtExpiresIn = 60 * 60 * 24 * 7 // 7 days
 export default {
     URL,
