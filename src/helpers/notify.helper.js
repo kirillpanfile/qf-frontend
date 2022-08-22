4708b293f80b1bdb6247d1cbcebecbf23f3e7456
RasslaBones <VladutuT@gmail.com>
2022-08-22 17:17:57 +0300

    Update notify.helper.js

--- src/helpers/notify.helper.js
+++ src/helpers/notify.helper.js
@@ -1,16 +1,5 @@
 import { notify } from '@kyvg/vue3-notification'
 
-const errStatus = {
-    400: 'Oops, something went wrong',
-    401: 'You are not authorized to access this resource',
-    403: 'You are not authorized to access this resource',
-    404: 'The resource you are looking for is not found',
-    500: 'Looks like we have a internal server error',
-    502: 'Looks like the server is down',
-    503: 'Looks like the server is sleeping',
-    504: 'Looks like the server is overloaded',
-}
-
 /**
  * Working with global Notifications
  * Notify success, error, warning, info
@@ -26,11 +15,10 @@ export const Notify = (message, type) => {
         if (!type) throw new Error('Notify type is not valid')
         else {
             if (type === 'error') {
-                const status = message?.response?.status
-                notify({ type: 'error', text: errStatus[status] })
-            } else notify({ type, text: message })
+                notify({ type: 'error', text: message.message})
+            } else notify({ type, text: message.message })
         }
     } catch (error) {
-        console.error('🚀 ~ file: globals.js ~ line 77 ~ notify ~ error', error)
+        console.error('Error',error)
     }
 }
